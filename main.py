fdaf91fc7e921270df59fd8923b046eae57ccaa2
Harshkumar1422 <harshtyagi140801@gmail.com>
2022-03-23 20:57:07 +0530

    configured db and

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import flask
-import mysql.connector
+from flask import Flask, render_template, request, redirect, session, flash
+from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+from flask_ckeditor import CKEditor
+import yaml
 
-mydb = mysql.connector.connect(host ="localhost" ,user = "navin" , passwd = "12345")
- 
+app = Flask(__name__)
+Bootstrap(app)
+ckeditor = CKEditor(app)
+
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+app.config['SECRET_KEY'] = 'secret'
+
+@app.route('/')
+def index():
+    
