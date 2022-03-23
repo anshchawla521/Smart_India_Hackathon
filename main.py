2e31485755852e67b7eabb19a7800ede6d757d32
Harshkumar1422 <harshtyagi140801@gmail.com>
2022-03-24 00:45:38 +0530

    created admin page and basic working website
    Co-authored-by: anshchawla521 <anshchawla521@users.noreply.github.com>
    Co-authored-by: Sukh0204 <Sukh0204@users.noreply.github.com>
    Co-authored-by: AshokSingla <AshokSingla@users.noreply.github.com>

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ mysql = MySQL(app)
 
 app.config['SECRET_KEY'] = 'secret'
 
+# home page 
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+
+# login and register pages for admin , user , hospital
 @app.route('/register_admin/', methods=['GET', 'POST'])
 def register_admin():
     if request.method == 'POST':
@@ -41,6 +44,8 @@ def register_admin():
 
 @app.route('/login_admin/', methods=['GET', 'POST'])
 def login_admin():
+    if session['login'] :
+        return redirect('/admin')
     if request.method == 'POST':
         userDetails = request.form
         username = userDetails['name']
@@ -50,8 +55,8 @@ def login_admin():
             user = cur.fetchone()
             if userDetails['password'] == user['password']:
                 session['login'] = True
-                session['Name'] = user['name']
-                flash('Welcome ' + session['Name'] +'! You have been successfully logged in', 'success')
+                session['admin_Name'] = user['name']
+                flash('Welcome ' + session['admin_Name'] +'! You have been successfully logged in', 'success')
             else:
                 cur.close()
                 flash('Password does not match', 'danger')
@@ -61,7 +66,8 @@ def login_admin():
             flash('User not found', 'danger')
             return render_template('login_admin.html')
         cur.close()
-        return redirect('/')
+        return redirect('/admin')
+    
     return render_template('login_admin.html')
 
 @app.route('/register_user/', methods=['GET', 'POST'])
@@ -91,8 +97,8 @@ def login_user():
             user = cur.fetchone()
             if userDetails['password'] == user['password']:
                 session['login'] = True
-                session['Name'] = user['name']
-                flash('Welcome ' + session['Name'] +'! You have been successfully logged in', 'success')
+                session['user_Name'] = user['name']
+                flash('Welcome ' + session['user_Name'] +'! You have been successfully logged in', 'success')
             else:
                 cur.close()
                 flash('Password does not match', 'danger')
@@ -133,8 +139,8 @@ def login_hospital():
             user = cur.fetchone()
             if userDetails['password'] == user['password']:
                 session['login'] = True
-                session['Name'] = user['name']
-                flash('Welcome ' + session['Name'] +'! You have been successfully logged in', 'success')
+                session['hospital_Name'] = user['name']
+                flash('Welcome ' + session['hospital_Name'] +'! You have been successfully logged in', 'success')
             else:
                 cur.close()
                 flash('Password does not match', 'danger')
@@ -147,7 +153,80 @@ def login_hospital():
         return redirect('/')
     return render_template('login_hospital.html')
 
+# admin page 
 
+@app.route('/admin/', methods=['GET', 'POST'])
+def admin():
+    
+    return render_template('admin.html')
+
+# pages for admin_addprice disease , equipmennts , medicine
+@app.route('/admin_addprice_disease/', methods=['GET', 'POST'])
+def admin_addprice_disease():
+    if request.method == 'POST':
+        userDetails = request.form
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO disease_gov(name,min_price,max_price,days_recover) "\
+        "VALUES(%s,%s,%s,%s)",(userDetails['name'], userDetails['min_price'], userDetails['max_price'], userDetails['days_recover']))
+        mysql.connection.commit()
+        cur.close()
+        flash('Data added to the database', 'success')
+        return redirect('/admin')
+    return render_template('admin_addprice_disease.html')
+
+@app.route('/admin_addprice_equipment/', methods=['GET', 'POST'])
+def admin_addprice_equipment():
+    if request.method == 'POST':
+        userDetails = request.form
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO equipment_gov(min_price,max_price,name) "\
+        "VALUES(%s,%s,%s)",( userDetails['min_price'], userDetails['max_price'], userDetails['name']))
+        mysql.connection.commit()
+        cur.close()
+        flash('Data added to the database', 'success')
+        return redirect('/admin')
+    return render_template('admin_addprice_equipment.html')
+
+@app.route('/admin_addprice_medicine/', methods=['GET', 'POST'])
+def admin_addprice_medicine():
+    if request.method == 'POST':
+        userDetails = request.form
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO medicine_gov(name,min_price,max_price) "\
+        "VALUES(%s,%s,%s)",(userDetails['name'], userDetails['min_price'], userDetails['max_price']))
+        mysql.connection.commit()
+        cur.close()
+        flash('Data added to the database', 'success')
+        return redirect('/admin')
+    return render_template('admin_addprice_medicine.html')
+
+
+@app.route('/user_addreport/', methods=['GET', 'POST'])
+def user_addreport():
+    if request.method == 'POST':
+        userDetails = request.form
+        print(userDetails['date'])
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO complain(complain, name, date) "\
+        "VALUES(%s,%s,%s)",(userDetails['complain'], userDetails['name'], userDetails['date']))
+        mysql.connection.commit()
+        cur.close()
+        flash('Your Complaint Registered', 'success')
+        return redirect('/admin_viewreport')
+    return render_template('user_addreport.html')
+
+@app.route('/admin_viewreport/', methods=['GET', 'POST'])
+def admin_viewreport():
+    
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM complain")
+    if resultValue > 0:
+        complains = cur.fetchall()
+        cur.close()
+        return render_template('admin_viewreport.html', complains=complains)
+    cur.close()
+    return render_template('admin_viewreport.html',compalins=None)
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
